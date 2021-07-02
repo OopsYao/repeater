740cc76af1ac0258d262f4576894e7c66b309e25
oopsy <whoops.yao@gmail.com>
2021-07-02 18:30:00 +0800

    add http server

--- bot.js
+++ bot.js
@@ -1,12 +1,6 @@
 const { Wechaty } = require("wechaty");
 const qt = require("qrcode-terminal");
-
-const masterAlias = "xy";
-
-const login = (bot) =>
-  new Promise((resolve) => {
-    bot.on("login", resolve);
-  });
+const http = require("http");
 
 const say = async (bot, query, message) => {
   const contact = await bot.Contact.find(query);
@@ -18,12 +12,22 @@ const sayRoom = async (bot, name, message) => {
   await room.say(message);
 };
 
-(async () => {
+const botStart = async () => {
+  const login = (bot) =>
+    new Promise((resolve) => {
+      bot.on("login", resolve);
+    });
+
   const bot = Wechaty.instance() // Global Instance
     .on("scan", (qrcode, status) => qt.generate(qrcode));
   await bot.start();
   await login(bot);
+  return bot;
+};
 
+(async () => {
+  const bot = await botStart();
+  const masterAlias = "xy";
   const master = await bot.Contact.find({ alias: masterAlias });
   bot.on("message", async (m) => {
     // TODO: Debouncing and forward messages together
@@ -34,4 +38,31 @@ const sayRoom = async (bot, name, message) => {
     if (await m.room().has(master)) return; // Including the common room with the master
     await m.forward(master);
   });
+
+  const requestListener = async (req, resp) => {
+    if (req.method === "POST") {
+      const bodyStr = await getBody(req);
+      const { contact, room, message } = JSON.parse(bodyStr);
+      if (contact) {
+        await say(bot, contact, message);
+      } else if (room) {
+        await sayRoom(bot, room, message);
+      }
+      resp.writeHead(201);
+      resp.end();
+    }
+  };
+  const getBody = (req) =>
+    new Promise((resolve) => {
+      let body = "";
+      req.on("data", (d) => {
+        body += d;
+      });
+      req.on("end", () => {
+        resolve(body);
+      });
+    });
+
+  const server = http.createServer(requestListener);
+  server.listen(8080);
 })();
